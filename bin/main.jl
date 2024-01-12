d562ae95a73bb18728da44c3f81a8aaace0f4c4a
Steve Kelly <kd2cca@gmail.com>
2024-01-12 14:20:05 -0500

    fix entry point

--- bin/main.jl
+++ bin/main.jl
@@ -3,4 +3,4 @@ using DashJuliaHubExample
 @info "Setup and now serving..."
 # Heroku passes the port as the first argument; JuliaHub as a PORT ENV var
 port = something(tryparse(Int, get(ARGS, 1, "")), tryparse(Int, get(ENV, "PORT", "")), 8080)
-DashJuliaHubExample(;port)
+DashJuliaHubExample.main(;port)
