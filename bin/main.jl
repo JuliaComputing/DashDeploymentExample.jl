028335071c45a3f978eec8f8d4e5bc88b90c6a88
Steve Kelly <kd2cca@gmail.com>
2024-01-16 11:28:24 -0500

    copy from projects

--- bin/main.jl
+++ bin/main.jl
@@ -1,12 +1,34 @@
-using DashJuliaHubExample
-using Sockets
 using Dash
 
-# Heroku passes the port as the first argument; JuliaHub as a PORT ENV var
-port = something(tryparse(Int, get(ARGS, 1, "")), tryparse(Int, get(ENV, "PORT", "")), 8080)
-app = DashJuliaHubExample.main(port)
-@info Sockets.localhost
-@info port
-@info app
-@info "Setup and now serving..."
-Dash.run_server(app, Sockets.localhost, port)
+# Enter 9999 in the port field in the deploy form when deploying on JuliaHub
+const PORT = 9999
+
+const DEV_MODE = haskey(ENV, "VSCODE_PROXY_URI")
+
+function run_app(host="0.0.0.0", port=PORT)
+    @info("Initializing dash...")
+    app = dash(;
+        external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"],
+        requests_pathname_prefix=(DEV_MODE ? "/proxy/$(string(port))/" : "/"),
+    )
+
+    app.layout = html_div() do
+        html_h1("Hello Dash"),
+        html_div("Dash.jl: Julia interface for Dash"),
+        dcc_graph(;
+            id="example-graph",
+            figure=(
+                data=[
+                    (x=[1, 2, 3], y=[4, 1, 2], type="bar", name="SF"),
+                    (x=[1, 2, 3], y=[2, 4, 5], type="bar", name="Montréal"),
+                ],
+                layout=(title="Dash Data Visualization",),
+            ),
+        )
+    end
+
+    @info("Starting server...")
+    run_server(app, host, port; debug=DEV_MODE)
+end
+
+run_app()
